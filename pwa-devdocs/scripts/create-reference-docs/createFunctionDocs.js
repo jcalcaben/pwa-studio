efceaa1284198d4006b9a0d49fe31ea0115ca6c3
James Calcaben <calcaben@adobe.com>
2021-03-24 10:44:26 -0500

    Add handlebar template overrides

--- pwa-devdocs/scripts/create-reference-docs/createFunctionDocs.js
+++ pwa-devdocs/scripts/create-reference-docs/createFunctionDocs.js
@@ -2,10 +2,18 @@
  * Uses the jsdoc-to-markdown library to generate function docs
  */
 const jsDocs = require('jsdoc-to-markdown');
+const path = require('path');
 
 let createFunctionDocs = ({ sourcePath, githubSource, childComponents = [] }) => {
     const files = [sourcePath, ...childComponents];
-    return jsDocs.render({ files: files }).then(content => {
+    const config = {
+        files: files,
+        partial: [
+            path.join(__dirname,'templates','handlebars','link.hbs'),
+            path.join(__dirname,'templates','handlebars','linked-type-list.hbs'),
+        ],
+    }
+    return jsDocs.render(config).then(content => {
         return new Promise((resolve, reject) => {
             if (!content) {
                 reject(Error(`Could not generate content for ${sourcePath}`, sourcePath));
